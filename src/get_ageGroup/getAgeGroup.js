774a1b73d96d944de7c87e9e63d5cde99e8cafde
Odin Thif <mjd.thif@icloud.com>
2024-09-04 21:20:46 +0200

    getAgeGroup set up

--- src/get_ageGroup/getAgeGroup.js
+++ src/get_ageGroup/getAgeGroup.js
@@ -1,3 +1,13 @@
 export function getAgeGroup(inputAge) {
-  return "toddler";
+  return inputAge < 0
+    ? "Not a valid input"
+    : inputAge <= 3
+    ? "toddler"
+    : inputAge <= 12
+    ? "kid"
+    : inputAge <= 19
+    ? "teenager"
+    : inputAge <= 39
+    ? "adult"
+    : "old";
 }
--- src/get_ageGroup/getAgeGroup.test.js
+++ src/get_ageGroup/getAgeGroup.test.js
@@ -2,11 +2,26 @@ import { describe, it, expect } from "vitest";
 import { getAgeGroup } from "./getAgeGroup";
 
 describe("age classifier", () => {
+  it("negative numbers should return not a valid input!", () => {
+    expect(getAgeGroup(-2)).toBe("Not a valid input");
+  });
+
   it("0-3 years old should be a toddler", () => {
     expect(getAgeGroup(0)).toBe("toddler");
   });
-  it("4-12 years old should be a kid");
-  it("13-19 years old should be a teenager");
-  it("20-39 years old should be a adult");
-  it("above 39 years old ... you are just old");
+  it("0-3 years old should be a toddler", () => {
+    expect(getAgeGroup(3)).toBe("toddler");
+  });
+  it("4-12 years old should be a kid", () => {
+    expect(getAgeGroup(12)).toBe("kid");
+  });
+  it("13-19 years old should be a teenager", () => {
+    expect(getAgeGroup(19)).toBe("teenager");
+  });
+  it("20-39 years old should be a adult", () => {
+    expect(getAgeGroup(39)).toBe("adult");
+  });
+  it("above 39 years old ... you are just old", () => {
+    expect(getAgeGroup(50)).toBe("old");
+  });
 });
